e151124fac223b743aede4b2d372d6af150f815b
MilaY0109 <yanovskyl@ukr.net>
2024-03-28 09:59:21 +0200

    task2

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import DrinksCounter from "./components/DrinksCounter/DrinksCounter";
+import DrinksValues from "./components/DrinksValues/DrinksValues";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [drinks, setDrinks] = useState({ beer: 0, whiskey: 0, wine: 0 });
+
+  const handleLogDrink = (drinkName) => {
+    // drinkName -> "beer" | "whiskey" | "wine"
+    if (drinks[drinkName] === 2 && drinkName === "beer") {
+      alert("Sorry, you excedded the beer limit. Please choose another drink!");
+      return;
+    }
+    setDrinks({ ...drinks, [drinkName]: drinks[drinkName] + 1 });
+  };
+
+  const handleIncrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrementCounter = () => {
+    if (counter === 0) return;
+
+    setCounter(counter - 1);
+  };
+
+  const drinksTotal = drinks.beer + drinks.whiskey + drinks.wine;
+
+  return (
+    <div>
+      <button onClick={handleIncrementCounter}>Counter: {counter}</button>
+      <button onClick={handleDecrementCounter}>-</button>
+      <DrinksValues drinks={drinks} total={drinksTotal} />
+      <DrinksCounter handleLogDrink={handleLogDrink} />
+    </div>
+  );
+}
+
+export default App;
